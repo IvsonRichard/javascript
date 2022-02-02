124fd67084fcd49a4e8d01a71f1b36e9a320b4cb
IvsonRichard <ivsonrsm@gmail.com>
2022-02-02 02:32:14 -0300

    js

---
+++ al004/ex004.js
@@ -0,0 +1,7 @@
+var vel = 80
+
+console.log(`a velocidade é ${vel}km/h`)
+if (vel > 60){// condição simples
+    console.log(`voce foi multado!`)
+}
+console.log(`boa noite`)
---
+++ al004/ex005.js
@@ -0,0 +1,7 @@
+var país ='brasil'
+console.log(`vivendo em ${país}`)
+if (país != "brasil"){
+    console.log(`você é estrangeiro`)
+} else{
+    console.log(` voce é do brasil`)
+}
